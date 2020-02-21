4bbf6b392937b80f28f1c13b9654f9888c6730a6
Mr-Morgan <zmorgan163@gmail.com>
2020-02-21 14:38:28 +0300

    Home work. Lesson 6

--- MyTextEditor/mainwindow.cpp
+++ MyTextEditor/mainwindow.cpp
@@ -1,27 +1,38 @@
 ﻿#include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#define ACTIVESUBWIN static_cast<MySubWindow*>(ui->mdiArea->activeSubWindow()->widget())
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
 
-    ui->toolBar->addAction(ui->actionFullScreen);
+    ui->toolBar->addAction(ui->actionNewWindow);
     ui->toolBar->addAction(ui->actionUndo);
     ui->toolBar->addAction(ui->actionRedo);
     ui->toolBar->addSeparator();
     ui->toolBar->addAction(ui->actionDayNight);
     ui->toolBar->addAction(ui->actionSettings);
+    ui->toolBar->addAction(ui->actionFullScreen);
     ui->toolBar->addAction(ui->actionAbout);
     ui->toolBar->addSeparator();
     ui->toolBar->addAction(ui->actionExit);
+
+    on_actionNewWindow_triggered();
 }//MainWindow(QWidget *parent = nullptr)
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }//~MainWindow()
+
+void MainWindow::setLabelsText(QString text)
+{
+    for (auto i : ui->mdiArea->subWindowList())
+        static_cast<MySubWindow*>(i->widget())->setLabelText(text);
+}//void setLabelsText(QString text)
 
 void MainWindow::on_actionFullScreen_triggered()
 {
@@ -32,28 +43,24 @@ void MainWindow::on_actionFullScreen_triggered()
         showFullScreen();
         ui->actionFullScreen->setIcon(QIcon(":/icons/icons/fullScreenOff.png"));
     }//if (isFullScreen())
-//    (this->isFullScreen())?this->showNormal():this->showFullScreen();
 }//on_actionFullScreen_triggered()
 
 void MainWindow::on_actionUndo_triggered()
 {
-    ui->textEdit->on_actionUndo_triggered();
+    ACTIVESUBWIN->undo();
 }//on_actionUndo_triggered()
 
 void MainWindow::on_actionRedo_triggered()
 {
-    ui->textEdit->on_actionRedo_triggered();
+    ACTIVESUBWIN->redo();
 }//on_actionRedo_triggered()
 
 void MainWindow::on_actionAbout_triggered()
 {
     QFile file(":/about.txt");
     if (file.open(QIODevice::ReadOnly | QIODevice::Text)) {
-        ui->textEdit->setReadOnly(false);
-        ui->lineEdit->setReadOnly(false);
-        ui->textEdit->setPlainText(QString(file.readAll()));
-        ui->textEdit->moveCursor(QTextCursor::Start);
-        ui->lineEdit->setText("Mr. Morgan");
+        QTextStream stream(&file);
+        ACTIVESUBWIN->printText(stream, true);
         file.close();
     }//if (file.open(QIODevice::ReadOnly | QIODevice::Text))
 }//on_actionAbout_triggered()
@@ -66,11 +73,8 @@ void MainWindow::on_open_triggered()
         QFile file(filename);
         if (file.open(QIODevice::ReadOnly | QIODevice::Text)) {
             QTextStream stream(&file);
-            ui->textEdit->setReadOnly(false);
-            ui->lineEdit->setReadOnly(false);
-            if (filename.right(7) == ".binary") ui->lineEdit->setText(stream.readLine());
-            ui->textEdit->setPlainText(QString(stream.readAll()));
-            ui->textEdit->moveCursor(QTextCursor::Start);
+            bool is_binary = (filename.right(7) == ".binary");
+            ACTIVESUBWIN->printText(stream, is_binary, false);
             file.close();
         }//if (file.open(..))
     } else QMessageBox::information(this, "Ошибка чтения файла", "Не удалось считать выбранный файл.");
@@ -82,10 +86,10 @@ void MainWindow::on_save_triggered()
                                                     QString("Текстовый файл(*.txt);;Бинарный файл(*.binary)"));
     if (filename.length()) {
         QFile file(filename);
-        if (file.open(QIODevice::WriteOnly | QIODevice::NewOnly)){
+        if (file.open(QIODevice::WriteOnly)){
             QTextStream stream(&file);
-            if (filename.right(7) == ".binary") stream << ui->lineEdit->text() << '\n';
-            stream << ui->textEdit->toPlainText();
+            bool is_binary = (filename.right(7) == ".binary");
+            ACTIVESUBWIN->scanText(stream, is_binary);
             file.close();
         }//if (file.open(..))
     }else QMessageBox::information(this, "Ошибка записи файла", "Не удалось записать данные в выбранный файл.");
@@ -93,9 +97,17 @@ void MainWindow::on_save_triggered()
 
 void MainWindow::on_open_read_only_triggered()
 {
-    emit ui->open->triggered();
-    ui->textEdit->setReadOnly(true);
-    ui->lineEdit->setReadOnly(true);
+    QString filename = QFileDialog::getOpenFileName(this, "Выберите файл для открытия", QDir::current().path(),
+                                                    QString("Текстовый файл(*.txt);;Бинарный файл(*.binary);;Все файлы(*.*)"));
+    if (filename.length()) {
+        QFile file(filename);
+        if (file.open(QIODevice::ReadOnly | QIODevice::Text)) {
+            QTextStream stream(&file);
+            bool is_binary = (filename.right(7) == ".binary");
+            ACTIVESUBWIN->printText(stream, is_binary, true);
+            file.close();
+        }//if (file.open(..))
+    } else QMessageBox::information(this, "Ошибка чтения файла", "Не удалось считать выбранный файл.");
 }//on_open_read_only_triggered()
 
 void MainWindow::on_actionExit_triggered()
@@ -108,7 +120,8 @@ void MainWindow::on_actionDayNight_triggered()
     if (NightMode) {
         ui->actionDayNight->setIcon(QIcon(":/icons/icons/classic.png"));
         qApp->setStyleSheet("");
-        ui->label->setText("<FONT COLOR=#000000>Author:</FONT>");
+        ui->mdiArea->setBackground(QBrush(QColor("#9C9C9C")));
+        setLabelsText("<FONT COLOR=#000000>Author:</FONT>");
         NightMode = false;
     } else {
         QFile file(":/night.css");
@@ -116,9 +129,31 @@ void MainWindow::on_actionDayNight_triggered()
             ui->actionDayNight->setIcon(QIcon(":/icons/icons/night.png"));
             QString style = file.readAll();
             qApp->setStyleSheet(style);
-            ui->label->setText("<FONT COLOR=#A6B4B5>Author:</FONT>");
+            ui->mdiArea->setBackground(QBrush(QColor("#374463")));
+            setLabelsText("<FONT COLOR=#A6B4B5>Author:</FONT>");
             file.close();
             NightMode = true;
         }//if (file.open(QIODevice::ReadOnly))
     }//if (NightMode)
 }//on_actionDayNight_triggered()
+
+void MainWindow::on_actionPrint_triggered()
+{
+    QPrinter p;
+    QPrintDialog pdlg(&p,this);
+    pdlg.setWindowTitle("Печать");
+    if (pdlg.exec() != QDialog::Accepted) return;
+    ACTIVESUBWIN->print(&p);
+}//on_actionPrint_triggered()
+
+void MainWindow::on_actionNewWindow_triggered()
+{
+    MySubWindow *subwindow = new MySubWindow(ui->mdiArea);
+    subwindow->setObjectName(QString::fromUtf8("subwindow"));
+
+    NightMode? subwindow->setLabelText("<FONT COLOR=#A6B4B5>Author:</FONT>"):
+               subwindow->setLabelText("<FONT COLOR=#000000>Author:</FONT>");
+
+    ui->mdiArea->addSubWindow(subwindow);
+    subwindow->show();
+}//on_actionNewWindow_triggered()
---
+++ MyTextEditor/mysubwindow.h
@@ -0,0 +1,34 @@
+#ifndef MYSUBWINDOW_H
+#define MYSUBWINDOW_H
+
+#include <QGridLayout>
+#include <QTextStream>
+#include <QLineEdit>
+#include <QPrinter>
+#include <QWidget>
+#include <QLabel>
+
+#include "mytextedit.h"
+
+class MySubWindow : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit MySubWindow(QWidget *parent = nullptr);
+    ~MySubWindow();
+    void undo();
+    void redo();
+    void printText(QTextStream &s, bool binary = false, bool readonly = false);
+    void scanText(QTextStream &s, bool binary = false);
+    void setLabelText(QString text);
+    void print(QPrinter *p);
+
+signals:
+
+private:
+    MyTextEdit *textEdit;
+    QLineEdit *lineEdit;
+    QLabel *label;
+};//class MySubWindow : public QWidget
+
+#endif // MYSUBWINDOW_H
